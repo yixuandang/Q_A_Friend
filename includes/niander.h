91ca9821af09eea1fb2ae074f48181ad7123ffd1
Yixuan D ang <ydang@e1z2r8p23.42.us.org>
2016-10-31 08:54:19 -0700

    the header file of my project

---
+++ includes/niander.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   niander.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydang <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/29 15:44:43 by ydang             #+#    #+#             */
+/*   Updated: 2016/10/29 15:44:45 by ydang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NIANDER_H
+# define NIANDER_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "./libft/includes/libft.h"
+
+void	ft_quesnans(char *s);
+void	ft_printerror(void);
+
+#endif
